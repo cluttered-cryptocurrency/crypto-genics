4d89e21208d076a1afbfb7ad7307e8638bc1540f
David Clutter <cluttered.code@gmail.com>
2017-08-29 16:27:15 -0600

    Add MarketManager

---
+++ src/main/java/com/cluttered/cryptocurrency/MarketManager.java
@@ -0,0 +1,104 @@
+package com.cluttered.cryptocurrency;
+
+import com.cluttered.cryptocurrency.ann.NeuralNetwork;
+import com.google.common.collect.EvictingQueue;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static com.cluttered.cryptocurrency.Action.HOLD;
+
+public interface MarketManager {
+
+    Logger LOG = LoggerFactory.getLogger(MarketManager.class);
+
+    String getMarket();
+
+    NeuralNetwork getNeuralNetwork();
+
+    BigDecimal getBalance();
+
+    void setBalance(final BigDecimal balance);
+
+    BigDecimal getShares();
+
+    void setShares(final BigDecimal shares);
+
+    BigDecimal getMinimumTrade();
+
+    BigDecimal getLastPrice();
+
+    void setLastPrice(final BigDecimal lastPrice);
+
+    void performBuyAction();
+
+    void performHoldAction();
+
+    void performSellAction();
+
+    BigDecimal getCommissionPercent();
+
+    void incrementBuyAttempts();
+
+    void incrementBuys();
+
+    void incrementSellAttempts();
+
+    void incrementSells();
+
+    void incrementSellHolds();
+
+    void incrementBuyHolds();
+
+    EvictingQueue<Double> getInputsQueue();
+
+    default Action fire(final MarketSummary input) {
+        setLastPrice(BigDecimal.valueOf(input.getLast()));
+
+        final EvictingQueue<Double> inputsQueue = getInputsQueue();
+        inputsQueue.add(input.getLast());
+
+        if(inputsQueue.remainingCapacity() > 0)
+            return HOLD;
+
+        final List<Double> inputs = Lists.newArrayList(inputsQueue);
+        final List<Double> outputs = getNeuralNetwork().fire(inputs);
+        return Action.determine(outputs);
+    }
+
+    default void performAction(final Action action) {
+        switch (action) {
+            case BUY:
+                incrementBuyAttempts();
+                if (getBalance().compareTo(getMinimumTrade()) < 0) {
+                    LOG.warn("Less than minimum required to perform buy");
+                    return;
+                }
+                // TODO: check if can purchase more shares
+                if (getShares().compareTo(BigDecimal.ZERO) == 0) {
+                    incrementBuys();
+                    performBuyAction();
+                } else incrementSellHolds();
+                break;
+            case SELL:
+                incrementSellAttempts();
+                if (getShares().multiply(getLastPrice()).compareTo(getMinimumTrade()) < 0) {
+                    LOG.warn("Less than minimum required to perform sell");
+                    return;
+                }
+                if(getShares().compareTo(BigDecimal.ZERO) > 0) {
+                    incrementSells();
+                    performSellAction();
+                } else incrementBuyHolds();
+                break;
+            default:
+                if(getShares().compareTo(BigDecimal.ZERO) == 0) {
+                    incrementBuyHolds();
+                } else incrementSellHolds();
+                performHoldAction();
+        }
+    }
+}
