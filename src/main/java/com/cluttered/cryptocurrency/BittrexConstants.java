bc5f5d2e73a8eab0a7cfcf827eda9b9778020de0
David Clutter <cluttered.code@gmail.com>
2017-08-30 01:25:54 -0600

    make bittrex constants an interface

--- src/main/java/com/cluttered/cryptocurrency/BittrexConstants.java
+++ src/main/java/com/cluttered/cryptocurrency/BittrexConstants.java
@@ -2,7 +2,7 @@ package com.cluttered.cryptocurrency;
 
 import java.math.BigDecimal;
 
-public class BittrexConstants {
+public interface BittrexConstants {
 
     BigDecimal MINIMUM_TRADE = BigDecimal.valueOf(0.0005);      // 50k satoshi
     BigDecimal COMMISSION_PERCENT = BigDecimal.valueOf(0.0025); // 0.25%
