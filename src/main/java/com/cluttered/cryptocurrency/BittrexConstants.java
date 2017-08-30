26b7f51710795e904470ead0e0cfceb91ef37602
David Clutter <cluttered.code@gmail.com>
2017-08-30 00:01:15 -0600

    Add Bittrex Constants

---
+++ src/main/java/com/cluttered/cryptocurrency/BittrexConstants.java
@@ -0,0 +1,9 @@
+package com.cluttered.cryptocurrency;
+
+import java.math.BigDecimal;
+
+public class BittrexConstants {
+
+    BigDecimal MINIMUM_TRADE = BigDecimal.valueOf(0.0005);      // 50k satoshi
+    BigDecimal COMMISSION_PERCENT = BigDecimal.valueOf(0.0025); // 0.25%
+}
